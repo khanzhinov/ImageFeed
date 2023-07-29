349a6856416c6f50df9f40b0b986112e52873e90
khanzhinov <khanzhinov@yandex.ru>
2023-07-29 19:31:06 +0300

    на ревью 2

--- ImagesListViewController.swift
+++ ImagesListViewController.swift
@@ -56,7 +56,7 @@ extension ImagesListViewController {
         cell.dateLabel.text = dateFormatter.string(from: Date())
         
         let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        let likeImage = isLiked ? UIImage(named: "like on") : UIImage(named: "like off")
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
